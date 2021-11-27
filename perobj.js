0c55d5bcef351cf5f6f08df6020fdfe0ef5af071
vivekankit198 <90336652+vivekankit198@users.noreply.github.com>
2021-11-27 22:30:45 +0530

    Add files via upload

---
+++ perobj.js
@@ -0,0 +1,22 @@
+var person1 = {
+    FirstName: "Vivek",
+    LastName: "Ankit",
+    OfficeAdress: "Gaya, Bihar 823001",
+};
+
+var person2 = {
+    FirstName: "Anjali",
+    LastName: "Kumari",
+    OfficeAdress: "Patna, Bihar 836281",
+};
+
+var person3 = {
+    FirstName: "Jayesh",
+    LastName: "Nigam",
+    OfficeAdress: "Mumbai, Maharashtra 400083",
+};
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
+
